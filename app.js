029d41681c9dbc41852b31d44ea114c932a1a3ee
jwala5 <dubeyshiva98@gmail.com>
2020-03-19 12:01:58 +0530

    Jwala Prasad Dubey <FSJ0320A004>

---
+++ app.js
@@ -0,0 +1,6 @@
+var wordfall = ["apple", "orange", "mango", "sweet", "banana", "tree", "shoe", "love", "friends",
+    "bread", "clothes", "market", "jet", "made", "great", "ocean", "country", "market", "history", "geography", "science",
+    "google", "brain", "spine", "nervous"
+];
+var big = Math.random(wordfall);
+console.log(big);
